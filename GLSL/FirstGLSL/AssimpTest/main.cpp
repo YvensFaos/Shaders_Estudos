d9b0ca91eb84b5e98cd86c231141c30a722a32f4
Yvens Rebouças Serpa <yvensre@gmail.com>
2015-04-14 13:33:11 -0300

    testando altas paradas

--- GLSL/FirstGLSL/AssimpTest/main.cpp
+++ GLSL/FirstGLSL/AssimpTest/main.cpp
@@ -1,8 +1,23 @@
 #include "assimp/Importer.hpp"
+#include "assimp/scene.h"
+#include "assimp/postprocess.h"
 
 int main()
 {
-	Assimp::Importer importer;
+	char* sFilePath = "path";
+
+	Assimp::Importer importer; 
+	const aiScene* scene = importer.ReadFile( sFilePath,  
+                               aiProcess_CalcTangentSpace       |  
+                               aiProcess_Triangulate            | 
+                               aiProcess_JoinIdenticalVertices  | 
+                               aiProcess_SortByPType); 
+
+	if(!scene) 
+	{ 
+		printf("Couldn't load model\n");
+		return -1; 
+	}
 
 	return 0;
 }
