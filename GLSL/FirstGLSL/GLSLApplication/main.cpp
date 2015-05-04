e66106c0c55cae21d124fbffde37990216f2d6a9
Yvens Notebook <yvensre@gmail.com>
2015-05-04 11:49:46 -0300

    de 2 a 6 para análise

--- GLSL/FirstGLSL/GLSLApplication/main.cpp
+++ GLSL/FirstGLSL/GLSLApplication/main.cpp
@@ -57,7 +57,7 @@ int main()
 	//Adiciona um texto adicional ao arquivo de logw
 	config.logExtraMsg = "test";
 	//Altura padrão de uma EDE
-	config.edeDepth = 1;
+	config.edeDepth = 6;
 	//EDE escolhida
 	config.type = SOCTREE;
 	//Colore os nós em tons diferente
