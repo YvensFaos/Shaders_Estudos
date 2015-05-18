20abed6ddf2a9b90a6bc2f239e997015f754099c
Yvens Notebook <yvensre@gmail.com>
2015-05-18 15:30:00 -0300

    fazendo os dinâmicos

--- GLSL/FirstGLSL/GLSLApplication/gldynamic.cpp
+++ GLSL/FirstGLSL/GLSLApplication/gldynamic.cpp
@@ -9,15 +9,47 @@ GLDynamic::GLDynamic(void)
 	standPosition = glm::vec3(0.0f, 0.0f, 0.0f);
 }
 
-GLDynamic::GLDynamic(char* modelPath, char* model3d, char* pathlocation, char* pathName)
+GLDynamic::GLDynamic(char* modelPath, char* model3d, glm::vec3 position)
 { 
 	if(GLBufferHandler::checkForMeshHandler(model3d))
 	{
+		handler = GLBufferHandler::meshHandlerBuffer[model3d];
+	}
+	else
+	{
 		handler = new GLMeshHandler(model3d, modelPath);
+		GLBufferHandler::addToMeshHandlerBuffer(model3d, handler);
 	}
 
-	index = -1;
-	moveable = false;
+	index = 0;
+	moveable = true;
+	standPosition = glm::vec3(position.x, position.y, position.z);
+}
+
+GLDynamic::GLDynamic(char* modelPath, char* model3d, char* pathLocation, char* pathName)
+{ 
+	if(GLBufferHandler::checkForMeshHandler(model3d))
+	{
+		handler = GLBufferHandler::meshHandlerBuffer[model3d];
+	}
+	else
+	{
+		handler = new GLMeshHandler(model3d, modelPath);
+		GLBufferHandler::addToMeshHandlerBuffer(model3d, handler);
+	}
+
+	if(GLBufferHandler::checkForPath(pathName))
+	{
+		steps = GLBufferHandler::pathBuffer[pathName];
+	}
+	else
+	{
+		readPathFile(pathLocation, pathName);
+		GLBufferHandler::addToPathBuffer(pathName, steps);
+	}
+
+	index = 0;
+	moveable = true;
 	standPosition = glm::vec3(0.0f, 0.0f, 0.0f);
 }
 
